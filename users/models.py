91ee51a75bb9418c7023b566f33df80beb919c25
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-03 18:46:02 +0330

    add user models

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    address = models.CharField(max_length=200)
+    national_code = models.CharField(max_length=20)
+    postal_code = models.CharField(max_length=20)
+    is_approved = models.BooleanField(default=False)
+
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def __str__(self):
+        if self.get_full_name:
+            return self.get_full_name
+        return self.username
