f8d5d64e2a0832885e6a56e8d2879a5cda458d9f
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-04 21:11:24 +0330

    add urls

--- bank_account/urls.py
+++ bank_account/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import CreateAccountView, AccountDetailView, AccountListView
+from .views import CreateAccountView, AccountDetailView, AccountListView, ChangeAccountActivation, ChangeAccountApproval
 
 urlpatterns = [
     path('me/add-account/', CreateAccountView.as_view(), name='add-account'),
-    path('list/', AccountListView.as_view(), name='add-account'),
-    path('me/detail/', AccountDetailView.as_view(), name='add-account'),
+    path('list/', AccountListView.as_view(), name='account-list'),
+    path('me/detail/', AccountDetailView.as_view(), name='account-detail'),
+    path('me/change-activation/', ChangeAccountActivation.as_view(), name='change-activation'),
+    path('me/change-approva/', ChangeAccountApproval.as_view(), name='change-approval'),
+    path('account-type/add-account-type/', AccountDetailView.as_view(), name='add-account'),
 ]
