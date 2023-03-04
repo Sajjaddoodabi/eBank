0d53e701e91948427fc147b7fec6c6db523a7428
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-04 21:52:22 +0330

    add create card view url

--- bank_account/urls.py
+++ bank_account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .views import CreateAccountView, AccountDetailView, AccountListView, ChangeAccountActivation, \
-    ChangeAccountApproval, AccountTypeList, AccountTypeDetail, ChangeAccountTypeActivation
+    ChangeAccountApproval, AccountTypeList, AccountTypeDetail, ChangeAccountTypeActivation, CreateAccountTypeView, CreateCardView
 
 urlpatterns = [
     path('me/add-account/', CreateAccountView.as_view(), name='add_account'),
@@ -9,7 +9,8 @@ urlpatterns = [
     path('me/detail/', AccountDetailView.as_view(), name='account_detail'),
     path('me/change-activation/', ChangeAccountActivation.as_view(), name='change_activation'),
     path('me/change-approva/', ChangeAccountApproval.as_view(), name='change_approval'),
-    path('account-type/add-account-type/', AccountDetailView.as_view(), name='add_account_type'),
+    path('me/add-card/', CreateCardView.as_view(), name='add_card'),
+    path('account-type/add-account-type/', CreateAccountTypeView.as_view(), name='add_account_type'),
     path('account-type/list/', AccountTypeList.as_view(), name='account_type_list'),
     path('account-type/detail/<int:pk>', AccountTypeDetail.as_view(), name='account_type_detail'),
     path('account-type/detail/change-activation/<int:pk>', ChangeAccountTypeActivation.as_view(), name='account_type_activation'),
