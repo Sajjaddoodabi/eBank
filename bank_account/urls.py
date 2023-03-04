3dee660e3e47cca1d40ba88c984c004426ef6048
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-04 21:07:15 +0330

    add account list and detail urls

--- bank_account/urls.py
+++ bank_account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import CreateAccountView
+from .views import CreateAccountView, AccountDetailView, AccountListView
 
 urlpatterns = [
-    path('add-account/', CreateAccountView.as_view(), name='add-account'),
+    path('me/add-account/', CreateAccountView.as_view(), name='add-account'),
+    path('list/', AccountListView.as_view(), name='add-account'),
+    path('me/detail/', AccountDetailView.as_view(), name='add-account'),
 ]
