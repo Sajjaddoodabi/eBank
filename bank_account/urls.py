d29720bb1fd7e897e943fbbb43cb16f981d9bfd4
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-04 21:18:15 +0330

    add account type detail and list urls

--- bank_account/urls.py
+++ bank_account/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-from .views import CreateAccountView, AccountDetailView, AccountListView, ChangeAccountActivation, ChangeAccountApproval
+from .views import CreateAccountView, AccountDetailView, AccountListView, ChangeAccountActivation, \
+    ChangeAccountApproval, AccountTypeList, AccountTypeDetail
 
 urlpatterns = [
-    path('me/add-account/', CreateAccountView.as_view(), name='add-account'),
-    path('list/', AccountListView.as_view(), name='account-list'),
-    path('me/detail/', AccountDetailView.as_view(), name='account-detail'),
-    path('me/change-activation/', ChangeAccountActivation.as_view(), name='change-activation'),
-    path('me/change-approva/', ChangeAccountApproval.as_view(), name='change-approval'),
-    path('account-type/add-account-type/', AccountDetailView.as_view(), name='add-account'),
+    path('me/add-account/', CreateAccountView.as_view(), name='add_account'),
+    path('list/', AccountListView.as_view(), name='account_list'),
+    path('me/detail/', AccountDetailView.as_view(), name='account_detail'),
+    path('me/change-activation/', ChangeAccountActivation.as_view(), name='change_activation'),
+    path('me/change-approva/', ChangeAccountApproval.as_view(), name='change_approval'),
+    path('account-type/add-account-type/', AccountDetailView.as_view(), name='add_account_type'),
+    path('account-type/list/', AccountTypeList.as_view(), name='account_type_list'),
+    path('account-type/detail/<int:pk>', AccountTypeDetail.as_view(), name='account_type_detail'),
 ]
