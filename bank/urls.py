d79a15608d867cb3ba66dc64dce5f10f2d248b51
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-07 18:55:58 +0330

    add transaction type detail url

--- bank/urls.py
+++ bank/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import CreateTransactionDestinationView, TransactionDestinationListAll, TransactionDestinationList, \
     ChangeTransactionDestinationActivation, ChangeTransactionDestinationValidation, TransactionDestinationDetailView, \
-    CreateTransactionTypeView, TransactionTypeListView
+    CreateTransactionTypeView, TransactionTypeListView, TransactionDetailView
 
 urlpatterns = [
     path('me/destination/create-destination/', CreateTransactionDestinationView.as_view(), name='create_destination'),
@@ -12,4 +12,5 @@ urlpatterns = [
     path('destination/change-validatoin/', ChangeTransactionDestinationActivation.as_view(), name='change_validation_destination'),
     path('type/add-type/', CreateTransactionTypeView.as_view(), name='add_type'),
     path('type/list/', TransactionTypeListView.as_view(), name='type_list'),
+    path('type/detail/<int:pk>', TransactionDetailView.as_view(), name='type_detail'),
 ]
