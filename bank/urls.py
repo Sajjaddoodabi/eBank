901172ec24a7aa50ce129ebcaf7c402710ec4d8d
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-07 16:32:50 +0330

    add add-transaction type url

--- bank/urls.py
+++ bank/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import CreateTransactionDestinationView, TransactionDestinationListAll, TransactionDestinationList, \
-    ChangeTransactionDestinationActivation, ChangeTransactionDestinationValidation, TransactionDestinationDetailView
+    ChangeTransactionDestinationActivation, ChangeTransactionDestinationValidation, TransactionDestinationDetailView, \
+    CreateTransactionTypeView
 
 urlpatterns = [
     path('me/destination/create-destination/', CreateTransactionDestinationView.as_view(), name='create_destination'),
     path('me/destination/list/', TransactionDestinationList.as_view(), name='my_destination_list'),
     path('me/destination/detail/<int:pk>', TransactionDestinationDetailView.as_view(), name='destination_detail'),
-    path('/destination/list/', TransactionDestinationListAll.as_view(), name='destination_list'),
-    path('/destination/change-activation/', ChangeTransactionDestinationValidation.as_view(), name='change_activation_destination'),
-    path('/destination/change-validatoin/', ChangeTransactionDestinationActivation.as_view(), name='change_validation_destination'),
+    path('destination/list/', TransactionDestinationListAll.as_view(), name='destination_list'),
+    path('destination/change-activation/', ChangeTransactionDestinationValidation.as_view(), name='change_activation_destination'),
+    path('destination/change-validatoin/', ChangeTransactionDestinationActivation.as_view(), name='change_validation_destination'),
+    path('type/add-type/', CreateTransactionTypeView.as_view(), name='add_type'),
 ]
