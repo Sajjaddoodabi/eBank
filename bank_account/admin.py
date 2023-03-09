6a4dda2a9970e9b14bae89b51fbc1fc92e00d2b9
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-09 17:08:53 +0330

    modify bank account admin

--- bank_account/admin.py
+++ bank_account/admin.py
@@ -11,7 +11,20 @@ class AccountAdmin(admin.ModelAdmin):
     inlines = [
         CardInline
     ]
+    list_display = ['user', 'type', 'balance', 'is_active', 'is_approved']
+    list_editable = ['is_active', 'is_approved']
+    list_filter = ['is_active', 'is_approved']
 
 
-admin.site.register(AccountType)
-admin.site.register(Card)
+@admin.register(AccountType)
+class AccountTypeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'is_active']
+    list_editable = ['is_active']
+    list_filter = ['is_active']
+
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ['account', 'card_number', 'cvv2', 'created_date', 'expire_date', 'is_active', 'is_ban']
+    list_editable = ['is_active', 'is_ban']
+    list_filter = ['is_active', 'is_ban']
