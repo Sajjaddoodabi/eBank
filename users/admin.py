00a00d163b7257c662c3fef9377ecec21b16d0c4
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-09 17:08:59 +0330

    modify user admin

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'mobile', 'is_active', 'is_approved', 'is_staff']
+    list_editable = ['is_active', 'is_approved']
+    list_filter = ['is_active', 'is_approved', 'is_staff', 'is_superuser']
