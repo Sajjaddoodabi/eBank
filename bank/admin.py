d0290acb7df0dfcf8708bbc690a68a8011539d59
Sajjaddoodabi <sajjaddoodabi55@gmail.com>
2023-03-09 13:04:16 +0330

    modify bank admin

--- bank/admin.py
+++ bank/admin.py
@@ -2,7 +2,29 @@ from django.contrib import admin
 from bank.models import TransactionType, TransactionWay, TransactionDestinationUser, Transaction
 
 
-admin.site.register(Transaction)
-admin.site.register(TransactionType)
-admin.site.register(TransactionWay)
-admin.site.register(TransactionDestinationUser)
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_filter = ['user', 'type', 'is_done', 'is_fail']
+    list_display = ['user', 'type', 'created_at', 'is_done', 'is_fail']
+    list_editable = ['is_done', 'is_fail']
+
+
+@admin.register(TransactionType)
+class TransactionTypeAdmin(admin.ModelAdmin):
+    list_filter = ['is_active']
+    list_display = ['title', 'is_active']
+    list_editable = ['is_active']
+
+
+@admin.register(TransactionWay)
+class TransactionWayAdmin(admin.ModelAdmin):
+    list_filter = ['is_active']
+    list_display = ['title', 'is_active']
+    list_editable = ['is_active']
+
+
+@admin.register(TransactionDestinationUser)
+class TransactionDestinationUserAdmin(admin.ModelAdmin):
+    list_filter = ['is_active', 'is_valid']
+    list_display = ['destination_name', 'card_number', 'is_active', 'is_valid']
+    list_editable = ['is_active', 'is_valid']
